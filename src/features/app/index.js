ffa1c65d82ede6c960c903d9f3ee4471f5d4caeb
Abdelkarim Ben Karroum <96283456+karroumBen@users.noreply.github.com>
2023-10-21 14:37:30 -0500

    Revert "Implementing nav bar"

--- src/features/app/index.js
+++ src/features/app/index.js
@@ -1,49 +1,14 @@
-import { Box, Center, Heading, Stack, Wrap, WrapItem } from '@chakra-ui/react'
 import React from 'react'
-import BidingItem from '../../components/BidingItem';
+import { ChakraProvider } from '@chakra-ui/react'
 
 
 const index = () => {
     return (
-        <section className="home-page">
-          <Heading fontSize='2xl' mb={2}>
-            Biding items
-          </Heading>
-
-
-          <Wrap spacing='15px'>
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-            <WrapItem>
-              <BidingItem />
-            </WrapItem>
-          </Wrap>
-        </section>
+      <ChakraProvider>
+        <div>
+          main app
+        </div>
+      </ChakraProvider>
     )
 }
 
