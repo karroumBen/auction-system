6e7fb1f7deccd9aa6255f8064be483470f18e73c
Abdelkarim Ben Karroum <garitomo@yahoo.fr>
2023-10-23 00:14:39 -0500

    refactored register and login page with chakra ui

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -1,34 +1,103 @@
 import React, { useState } from 'react';
-import './Login.css';
+import {
+  ChakraProvider,
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
-const Login = ({ onLogin, onNavigateToRegistration }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const Login = () => {
+  const [credentials, setCredentials] = useState({ email: '', password: ''});
 
-    const handleLogin = () => {
-        // Handle login logic here
-        onLogin({ email, password });
-    };
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(credentials);
+  };
 
-    return (
-        <div className="login-container">
-            <h2>Login</h2>
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="input-field" />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="input-field"
-            />
-            <button onClick={handleLogin} className="login-button">
-                Login
-            </button>
-            <p className="register-link" onClick={onNavigateToRegistration}>
-                Don't have an account? Register here
-            </p>
-        </div>
-    );
-};
+  const handleInputChange = (e) => {
+    setCredentials({...credentials, [e.target.name]: e.target.value})
+  }
+  return (
+    <ChakraProvider>
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+          </Stack>
+    
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={credentials.email}
+                  onChange={handleInputChange}/>
+              </FormControl>
+
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={credentials.password}
+                  onChange={handleInputChange}
+                />
+              </FormControl>
+
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}>
+                  <Checkbox>Remember me</Checkbox>
+                </Stack>
+
+                <Button
+                  onClick={handleLogin} 
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Sign in
+                </Button>
+              </Stack>
+
+              <Text fontSize={'md'} color={'gray.600'}>
+                Don't have an account? &nbsp;
+                <Link href='/register' color={'blue.400'}>
+                  Register <ExternalLinkIcon mx='2px' />
+                </Link>
+              </Text>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    </ChakraProvider>
+  )
+}
 
 export default Login;
