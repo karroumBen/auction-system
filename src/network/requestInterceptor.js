f7b268f3f6f4f66d6332e1011015cd4aa61960e5
Abdelkarim Ben Karroum <garitomo@yahoo.fr>
2023-10-24 21:44:37 -0500

    type fixed

--- src/network/requestInterceptor.js
+++ src/network/requestInterceptor.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
-axios.interceptors.request.use(function (config) {
-  const accessToken = localStorage.getItem('token');
-  if(accessToken) {
-    config.headers =  { Authorization: `Bearer ${accessToken}` };
-  }
 
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
+
+(() => {
+  axios.interceptors.request.use(function (config) {
+    const accessToken = localStorage.getItem('accessToken');
+    console.log(accessToken);
+    if(accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+  
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+})();
